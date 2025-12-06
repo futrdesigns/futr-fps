b18fded522e5cfb1b45799b3dae22d12be7af9e1
futrdesigns <99038220+futrdesigns@users.noreply.github.com>
2025-12-06 15:06:17 +0000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+
+game 'gta5'
+author 'futrdesigns'
+name "futr-fps"
+description "Modern FPS optimization menu with interactive UI"
+version '1.0.0'
+
+lua54 'yes'
+
+client_scripts {
+    'client/main.lua',
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
